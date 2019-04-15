6569ae3bbdecdfe03ceeda4af41f409ba0fabbf5
Jasper van Nieuwenhuizen <jaspervannieuwenhuizen@gmail.com>
2019-04-15 09:15:36 +0200

    Fix: update used TLS protocol to TLSv1.2

--- shotgun_api3/lib/httplib2/__init__.py
+++ shotgun_api3/lib/httplib2/__init__.py
@@ -73,7 +73,7 @@ try:
             cert_reqs = ssl.CERT_NONE
         else:
             cert_reqs = ssl.CERT_REQUIRED
-        # We should be specifying SSL version 3 or TLS v1, but the ssl module
+        # We should be specifying SSL version 3 or TLS v1.2, but the ssl module
         # doesn't expose the necessary knobs. So we need to go with the default
         # of SSLv23.
         return ssl.wrap_socket(sock, keyfile=key_file, certfile=cert_file,
